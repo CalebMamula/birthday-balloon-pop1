63ca30b79c1a248b866c95c56de6047dae06ca38
CalebMamula <128323622+CalebMamula@users.noreply.github.com>
2023-05-05 14:51:27 -0600

    Update customBlock3.ts

--- customBlock3.ts
+++ customBlock3.ts
@@ -3,6 +3,28 @@
 namespace Functions {
     //% block="testing"
     export function balloonAnimation(): void {
-        
+     
+    myDart = darts.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . c . . . . . . . . 
+        . . . . . . . c . . . . . . . . 
+        . . . . . . . c . . . . . . . . 
+        . . . . . . . c . . . . . . . . 
+        . . . . . . 6 c 6 . . . . . . . 
+        . . . . . . 6 6 6 . . . . . . . 
+        . . . . . . 6 6 6 . . . . . . . 
+        . . . . . . 6 6 6 . . . . . . . 
+        . . . . . 5 6 6 6 5 . . . . . . 
+        . . . . 5 5 2 2 2 5 5 . . . . . 
+        . . . . 5 5 6 6 6 5 5 . . . . . 
+        . . . . 5 5 2 2 2 5 5 . . . . . 
+        . . . . 5 . . . . . 5 . . . . . 
+        `, SpriteKind.Player, 80, 110)
+    myDart.controlWithArrowKeys()
+    myDart.setTrace()
+    myDart.angle = 75
+    myDart.angle += 15
     }
 }
